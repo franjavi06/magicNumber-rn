b7b9bf4bef67180fb07e95d66d132bcb46090516
franjavi06 <franjavi06@gmail.com>
2020-11-18 17:07:43 -0400

    images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from  'react-native';
+import {View, Text, StyleSheet, Button, Image} from  'react-native';
+
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over!</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
+            <TitleText>The Game is Over!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image 
+                //source={require('../assets/success.png')}
+                source={{uri:'https://n1.freetls.fastly.net/hubpage/dictionary-images/summit_3x.jpg'}}
+                 style={styles.image}
+                 />
+            </View>
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onRestartGame}/>
         </View>
     )
@@ -17,6 +27,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
